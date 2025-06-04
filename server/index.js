c2d057801d58d427e5afc9a4a3085d3f94603836
Utkarsh Rai <utkarsh.inventurs@gmail.com>
2025-06-04 19:11:11 +0530

    everything is fixed

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/product', require('./routes/product'));
 app.use('/api/cart', require('./routes/cart'));
-app.use('/api/razorpay', require('./routes/razorpay'));
+// app.use('/api/razorpay', require('./routes/razorpay'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
